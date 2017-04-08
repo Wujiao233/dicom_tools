d3c20f71c459875e10302479c78a06fcb91b4d26
Wujiao233 <wqh.1996@gmail.com>
2017-04-09 00:24:33 +0800

    Fix format problem

--- sort.py
+++ sort.py
@@ -3,13 +3,13 @@ import os
 import sys
 
 
-def get_path_filelist(path,filter=None):
+def get_path_filelist(path, filter=None):
     files = os.listdir(path)
     file_list = []
     for f in files:
         if (os.path.isfile(path + '/' + f)):
             if filter is None:
-            # 添加文件
+                # 添加文件
                 file_list.append(f)
             else:
                 if filter in f:
@@ -17,17 +17,18 @@ def get_path_filelist(path,filter=None):
 
     return file_list
 
+
 while True:
     path = input('Path:')
     filter_name = '.dcm'
 
     if os.path.isdir(path):
-        file_list = get_path_filelist(path,filter_name)
+        file_list = get_path_filelist(path, filter_name)
         # print(file_list)
         deep_and_filename = {}
         for f in file_list:
             dicom_tmp = dicom.read_file(path + '/' + f)
-            deep_and_filename.setdefault(float(dicom_tmp.SliceLocation),f);
+            deep_and_filename.setdefault(float(dicom_tmp.SliceLocation), f)
 
         deep_list = list(deep_and_filename.keys())
         deep_list.sort(reverse=True)
