0cbf45345163f4257e9a5095b628f246087e2729
Wujiao233 <wqh.1996@gmail.com>
2017-04-11 21:09:16 +0800

    Turn play to a module

--- play.py
+++ play.py
@@ -2,39 +2,56 @@ import dicom
 import sys
 import pylab
 import os
+import tqdm
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
 
-def get_path_filelist(path, filter=None):
-    files = os.listdir(path)
-    file_list = []
-    for f in files:
-        if (os.path.isfile(path + '/' + f)):
-            if filter is None:
-                # 添加文件
-                file_list.append(f)
-            else:
-                if filter in f:
+def play(array):
+    fig = plt.figure()
+    im = plt.imshow(array[0], vmin=-2000, vmax=4095, cmap=pylab.cm.bone)
+
+    def animate(i):
+        im.set_array(array[i])
+        return [im]
+
+    anim = animation.FuncAnimation(fig, animate, frames=len(array), interval=60, blit=True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    def get_path_filelist(path, filter=None):
+        files = os.listdir(path)
+        file_list = []
+        for f in files:
+            if os.path.isfile(path + '/' + f):
+                if filter is None:
+                    # 添加文件
                     file_list.append(f)
+                else:
+                    if filter in f:
+                        file_list.append(f)
 
-    return file_list
-
-
-path = sys.argv[1]
-file_list = get_path_filelist(path)
-file_list.sort(key=lambda x: int(str(x).replace(".dcm", "")))
-print(file_list)
-
-fig = plt.figure()
-di = dicom.read_file(path + '/0.dcm')
-im = plt.imshow(di.pixel_array,vmin=-2000, vmax=4095, cmap=pylab.cm.bone)
+        return file_list
 
 
-def animate(i):
-    im.set_array(dicom.read_file(path + '/' + file_list[i]).pixel_array)
-    return [im]
+    def read_dcm_files(filelist, path):
+        print("Reading files:")
+        slices = [dicom.read_file(path + "/" + f) for f in tqdm.tqdm(filelist)]
+
+        # Sort the dicom slices in their respective order
+        slices.sort(key=lambda x: int(x.InstanceNumber))
+
+        # Get the pixel values for all the slices
+        slices = np.stack([s.pixel_array for s in slices])
+        slices[slices == -2000] = 0
+        return slices
 
 
-anim = animation.FuncAnimation(fig, animate, frames=len(file_list), interval=10, blit=True)
-plt.show()
+    path = sys.argv[1]
+    file_list = get_path_filelist(path)
+    file_list.sort(key=lambda x: int(str(x).replace(".dcm", "")))
+    print(file_list)
+    arr = read_dcm_files(file_list, path)
+    play(arr)
